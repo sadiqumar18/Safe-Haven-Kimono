e89fbe59da212f8703da61ed609100f77f8516e4
abubakarumar <sadiqumar18>
2021-11-29 16:43:30 +0100

    add port

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from "@nestjs/common";
 import { useContainer } from "class-validator";
 import { TerminalModule } from "./terminal/terminal.module";
+import { AppConfig } from '../app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   useContainer(app.select(TerminalModule), {fallbackOnErrors: true});
-  await app.listen(3000);
+  await app.listen(AppConfig.PORT);
 
 }
 bootstrap();
